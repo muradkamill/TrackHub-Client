f917523099d515935734692774dee066ba1eb30f
muradkamil <muradkamilllll@gmail.com>
2025-12-09 22:19:28 +0400

    http

--- src/app/services/globalvar.ts
+++ src/app/services/globalvar.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class Globalvar {
-  BaseUrl: string = 'https://trackhub.runasp.net/api';
-  BaseUrlForImg: string = 'https://trackhub.runasp.net';
+  BaseUrl: string = 'http://trackhub.runasp.net/api';
+  BaseUrlForImg: string = 'http://trackhub.runasp.net';
 
 }
